200ee4f2c325cfd6095c840d5595b15a61bd5964
Aravind Kumar P <aravind.panneer@gmail.com>
2025-12-19 13:44:20 +0530

    deep into functional component and JSX

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-//const heading  = React.createElement("h1", {id:"heading"}, "Hello World From Namaste React!!!");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const Parent = () => (<h1>This is Jsx file</h1>);
 
-const parent = React.createElement("div", {id:"parent"},[
-         React.createElement("div", {id:"child1"},
-        [React.createElement("h1", {}, "I am h1 tag from child 1"),
-        React.createElement("h2", {}, "I am h2 tag from child 1")]
-    ),
-     React.createElement("div", {id:"child2"},
-        [React.createElement("h1", {}, "I am h1 tag from child 1"),
-        React.createElement("h2", {}, "I am h2 tag from child 1")]
-    ),
-])
+const ParentComponent = () => (
+ <div>  
+    <Parent /> 
+    {Parent()}
+<h2>This is react componenet</h2>
+<h1>this is another h1</h1>
+</div>
+)
 
 
 const root     = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<ParentComponent />);
+
--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # Namaste React
-
 git init 
 git remote add origin https://github.com/aravindkkk/aravind-react1
+npm init
+npm install -D parcel
+npm install react
+npm install react-dom
+to start => npx parcel index.html
+
+npm : install the package
+npx : executing the parcel
+
+Q: What is use of Parcel ?
+* Hot Module Replacement (HMR):
+It means that parcel will keep a track of all the files which you are updating 
+* parcel-cache:
+Parcel caches code all the time.when you run the application
+*dist:
+It keeps the files minified for us.
+Its comprises of all the compiled modules for production build.
+*Cleaning our code
+*Image optimization
+*Differentail bunding => Its means its support all the browser even old version also 
+*error handling 
+*Tree Shcking => Remove unused code
+
+pls refer : https://parceljs.org/
+ 
+
+
+
+
+
+
+
+
 
